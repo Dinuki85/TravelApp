9ebe37268db2131d0931407db61aaf6d5e3630ca
Dinuki85 <prarthanadinuki2@gmail.com>
2025-04-25 18:29:51 +0530

    added overaly in the mobileNav.tsx file

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 const MobileNav = () => {
   return (
     <div>
+        {/*overlay*/}
+        <div className='fixed inset-0 transform transtion-all duration-500 z-[1000] bg-black opacity-70 w-full h-screen'>
+            
+        </div>
       
     </div>
   )
