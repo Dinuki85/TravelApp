af07b1d11162bf9c1019fc6d4ede1eb4617c782d
Dinuki85 <prarthanadinuki2@gmail.com>
2025-04-25 18:27:13 +0530

    Added buttons and burger menu in Nav.tsx

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -2,10 +2,11 @@ import { navLinks } from '@/constant/constant';
 import React from 'react'
 import Link from 'next/link';
 import { MdTravelExplore } from 'react-icons/md';
+import { HiBars3BottomRight } from 'react-icons/hi2';
 
 const Nav = () => {
   return (
-    <div className='bg-blue-200 transition-all duration-100 h-[12vh] z-[1000]'>
+    <div className='bg-blue-600 transition-all duration-100 h-[12vh] z-[1000]'>
         <div className='flex items-center h-ful justify-between w-[90%] xl:w-[80%] mx-auto'>
             {/*logo*/}
             <div className='flex items-center space-x-2'>
@@ -20,15 +21,24 @@ const Nav = () => {
             <div className='hidden lg:flex items-center space-x-10'>
                 {navLinks.map((link)  =>{
                     return <Link href={link.url} key={link.id}>
-                        <p className="relative text-black text-base font-medium w-fit block after: block after:content-
+                        <p className="relative text-black text-base font-medium w-fit block after:block after:content-
                            [''] after:absolute after:h-[3px] after:bg-yellow-300 after:w-function  after:scale-x-0 after:hover:scale-x-100  after:transition duration-300 after:origin-right">
                       {link.label}</p>
                          </Link>
                 })}
+            </div>
+            {/*Butons*/}
+            <div className='flex items-center space-x-4'>
+                <button className='md:px-12 md:py-2.5 px-8 py-2 text-black text-base bg-white hover:bg-gray-200 transition-all duration-200 rounded-lg'>
+                    Book Now
+                </button>
+                {/*Burger Menu*/}
+                <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-white lg:hidden'/>
+
             </div>
         </div>
     </div>
   )
 }
 
-export default Nav
+export default Nav;
