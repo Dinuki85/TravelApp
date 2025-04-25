bf61737794702942644a5dece93f12777e2585f3
Dinuki85 <prarthanadinuki2@gmail.com>
2025-04-25 17:53:43 +0530

    Added designs for the Nav bar and added logo designs

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { TbAirBalloon } from 'react-icons/tb'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='bg-blue-200 transition-all duration-100 h-[12vh] z-[1000]'>
+        <div className='flex items-center h-ful justify-between w-[90%] xl:w-[80%] mx-auto'>
+            {/*logo*/}
+            <div className='flex items-center space-x-2'>
+                <div className='w-10 h-10 bg-rose-200 rounded-full flex items-center justify-center flex-col'>
+                    <TbAirBalloon className='w-6 h-6 text-white'/>
+
+                </div>
+                <h1 className='text-xl md:text-2xl text-white uppercase font-bold'>Tripi</h1>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
