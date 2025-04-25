3b68feb166177477c46d694519e2a6e5b66c2aa2
Dinuki85 <prarthanadinuki2@gmail.com>
2025-04-25 18:48:22 +0530

    Converted thsi component into a client component with useState

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import Nav from './Nav'
 import MobileNav from './MobileNav'
 
 const ResponsiveNav = () => {
+    const [showNav, setShowNav] = useState(false);
+
   return (
     <div>
         <Nav/>
