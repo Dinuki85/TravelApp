97525b4650624aadd1b17a252c7e01d1e88057c0
Dinuki85 <prarthanadinuki2@gmail.com>
2025-04-25 19:10:09 +0530

    added mobile Nav responses to the mobilenavigation

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -7,13 +7,14 @@ type Props={
     closeNav:()=> void;
 }
 
-const MobileNav = ({closeNav, showNav}:Props) => {
-  return (
+const MobileNav = ({ closeNav, showNav}:Props) => {
+    const navOpen = showNav?"translate-x-0":"translate-x-[-100%]";
+      return (
     <div>
         {/*overlay*/}
-        <div className='fixed inset-0 transform transtion-all duration-500 z-[1002] bg-black opacity-70 w-full h-screen'>
+        <div className={`fixed ${navOpen} inset-0 transform transtion-all duration-500 z-[1002] bg-black opacity-70 w-full h-screen`}>
           {/*navlinks*/}
-          <div className='text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[50%] sm:w[40%] bg-rose-700 space-y-6 z-[1050] '>
+          <div className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[50%] sm:w[40%] bg-rose-700 space-y-6 z-[1050] `}>
             {navLinks.map((link) =>{
                 return <Link key={link.id} href={link.url}>
                     <p className='text-white w-fit text-[20px] ml-12 border-b-[1,5px] pb-1 border-white sm:text[30px]'>
@@ -22,7 +23,7 @@ const MobileNav = ({closeNav, showNav}:Props) => {
                 </Link>
             })}
              {/*close button*/ }
-            <CgClose onClick={closeNav} className=' absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6'/>
+            <CgClose  onClick={closeNav} className=' absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6'/>
             </div>  
            
         </div>
