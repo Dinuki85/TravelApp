fd34780272765cb958a93b6e19ea76001850ab84
Dinuki85 <prarthanadinuki2@gmail.com>
2025-04-27 12:13:22 +0530

    Added review section to the home page

--- components/Home/WhyChoose/WhyChooseCrad.tsx
+++ components/Home/WhyChoose/WhyChooseCrad.tsx
@@ -9,6 +9,7 @@ type Props = {
 const WhyChooseCrad = ({ image, title }: Props) => {
   return (
     <div>
+        {/*Image*/}
       <Image
         src={image}
         width={70}
@@ -16,6 +17,11 @@ const WhyChooseCrad = ({ image, title }: Props) => {
         alt="image"
         className="mx-auto "
       />
+      {/*Content*/}
+      <h1 className="mt-6 text-center text-gray-900 font-medium text-lg">{title}</h1>
+      <p className="mt-2 text-center text-xs font-medium text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto fugit ipsa nisi sapiente voluptas consectetur incidunt odit id cum eius? Atque magni quod ea modi! Magnam nisi error quasi. Accusamus.</p>
+
+
     </div>
   );
 };
