a1b96513a12e4307e071c0cfa1b419ce7dfd1109
Dinuki85 <prarthanadinuki2@gmail.com>
2025-04-27 09:26:14 +0530

    Added DestinationSlider page

--- components/Home/Destination/Destination.tsx
+++ components/Home/Destination/Destination.tsx
@@ -1,13 +1,17 @@
 import SectionHedaing from '@/components/Helper/SectionHedaing'
 import React from 'react'
+import DestinationSlider from './DestinationSlider';
 
 const Destination = () => {
   return (
     <div className='pt-20 pb-20'>
         {/*Section Heading*/}
         <SectionHedaing  heading='Exploring Most Popular Destination'/>
-
-      
+        {/*Section content*/}
+        <div className='mt-14 w-[80%] mx-auto'>
+            {/*Slider*/}
+            <DestinationSlider/>
+        </div>
     </div>
   )
 }
---
+++ components/Home/Destination/DestinationSlider.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DestinationSlider = () => {
+  return (
+    <div >
+      
+    </div>
+  )
+}
+
+export default DestinationSlider
