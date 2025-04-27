f137375c7ed763016d7cf21ee68a035b77a2733b
Dinuki85 <prarthanadinuki2@gmail.com>
2025-04-27 12:43:52 +0530

    Added review  section with review texts

--- components/Home/Reviews/ReviewSlider.tsx
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+'use client'
+import React from 'react';
+
+import {Swiper, SwiperSlide} from 'swiper/react';
+import "swiper/css" ;
+import "swiper/css/effect-cards";
+
+import {EffectCards} from "swiper/modules";
+import { reviewData } from '@/data/data';
+
+
+
+
 
 const ReviewSlider = () => {
   return (
-    <div>ReviewSlider</div>
+    <div>
+        <Swiper effect={"cards"} grabCursor={true} modules={[EffectCards]} className='md:w-[450px] md:h-[350px] w-[90%] h-[300px]'>
+            {reviewData.map((data)=>{
+                return (
+                    <SwiperSlide key={data.id} className="bg-white rounded-3xl  block" >
+                        <div className='w-[80%] mx-auto mt-16'>
+                            {/*Review text */}
+                            <p className='text-xs sm:text-small md:text-base font-semibold'>{data.review}</p>
+                                
+                            </div>
+
+                    
+                    </SwiperSlide>
+                )
+            })}
+
+        </Swiper>
+
+    </div>
   )
 }
 
